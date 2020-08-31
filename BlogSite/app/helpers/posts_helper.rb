ea4338ee83576bbf173099e65aff29b2dfec355f
Kexin Xu <kexin@NB-Admins-MacBook-Pro.local>
2020-08-30 23:20:20 -0400

    update

--- BlogSite/app/helpers/posts_helper.rb
+++ BlogSite/app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def post_meta post
-    "Created by #{ post.account.first_name } #{ time_ago_in_words(post.created_at) } ago within #{ post.category.title }."
+    # "Created by #{ post.account.first_name } #{ time_ago_in_words(post.created_at) } ago within #{ post.category.title }."
     # Post has been viewed #{post.views} times.
   end
 end
